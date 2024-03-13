9c6879e1870ae8fae7e546c83dab527851ec118d
LiangC <liang.chen@mile.cloud>
2024-03-13 16:27:20 +0800

    feat: implement phase two features, having lp1 and lp2

--- src/SimpleSwap.sol
+++ src/SimpleSwap.sol
@@ -3,13 +3,13 @@ pragma solidity 0.8.20;
 import "./TestERC20.sol";
 
 contract SimpleSwap {
-  // phase 1
+  ///// Phase 1 /////
   TestERC20 public token0;
   TestERC20 public token1;
 
-  // phase 2
-  // uint256 public totalSupply = 0;
-  // mapping(address => uint256) public share;
+  ///// Phase 2 /////
+  uint256 public totalSupply = 0;
+  mapping(address => uint256) public share;
 
   constructor(address _token0, address _token1) {
     // 送進來的是 address，先轉換成 TestERC20 類型後儲存
@@ -17,7 +17,7 @@ contract SimpleSwap {
     token1 = TestERC20(_token1);
   }
 
-  // phase 1
+  ///// Phase 1 /////
   function addLiquidity1(uint256 _amount) public {
     // 從呼叫者（msg.sender）的賬戶中轉移 `_amount` 數量的 `token0`、`token1` 到當前合約地址（SimpleSwap address）
     // 可以說是將 token0, token1 的 SimpleSwap address -> _balance 增加 _amount
@@ -43,10 +43,27 @@ contract SimpleSwap {
     token1.transfer(msg.sender, token1.balanceOf(address(this)));
   }
 
-  // phase 2
-  // function addLiquidity2(uint256 _amount) public {
-  // }
+  ///// Phase 2 /////
+  function addLiquidity2(uint256 _amount) public {
+    token0.transferFrom(msg.sender, address(this), _amount);
+    token1.transferFrom(msg.sender, address(this), _amount);
+    // 記錄下目前總共的流動性
+    totalSupply += _amount;
+    // 記錄目前流動性提供者所投入的流動性
+    share[msg.sender] += _amount;
+  }
 
-  // function removeLiquidity2() public {
-  // }
+  function removeLiquidity2() public {
+    // 計算出 msg.sender 可以拿出的 token0、token1 數量，需要依據 share/totalSupply 的比例計算。須注意過程中不要有小數計算。
+    uint256 token0RemoveAmount = token0.balanceOf(address(this)) * share[msg.sender] / totalSupply;
+    uint256 token1RemoveAmount = token1.balanceOf(address(this)) * share[msg.sender] / totalSupply;
+
+    // 當前合約將該轉出的數量轉給 msg.sender
+    token0.transfer(msg.sender, token0RemoveAmount);
+    token1.transfer(msg.sender, token1RemoveAmount);
+
+    // totalSupply 扣出 msg.sender 取回的部分，並且記得將 msg.sender 的 share 歸零
+    totalSupply -= share[msg.sender];
+    share[msg.sender] = 0;
+  }
 }
